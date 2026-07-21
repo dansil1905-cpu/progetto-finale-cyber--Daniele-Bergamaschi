a2bc176a984ab55983ac1fcb8b8bfab0dd4fc5bb
dansil1905-cpu <dan.sil1905@gmail.com>
2026-07-21 17:02:03 +0200

    creazione model

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    Schema::create('posts', function (Blueprint $table) {
+    $table->id();
+    $table->string('title');
+    $table->text('body');
+    $table->string('status')->default('pending');
+    $table->foreignId('user_id')->constrained()->onDelete('cascade');
+    $table->timestamps();
+});
+}
