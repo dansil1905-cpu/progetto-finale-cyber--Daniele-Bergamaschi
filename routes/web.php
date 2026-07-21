4b3f8eb7467d0ee89978a99d59d8ea0a3c037912
dansil1905-cpu <dan.sil1905@gmail.com>
2026-07-21 17:42:11 +0200

    gestione middleware e scritte le rotte

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
-<?php
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\DashboardController;
 
-use Illuminate\Support\Facades\Route;
+// Rotte Pubbliche
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 
-Route::get('/', function () {
-    return view('welcome');
+// Rotte per Utenti Autenticati (Scrittori)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+});
+
+// Rotte protette per Revisori
+Route::middleware(['auth', 'revisore'])->group(function () {
+    Route::get('/reviser/dashboard', [DashboardController::class, 'reviserDashboard'])->name('reviser.dashboard');
+    Route::post('/reviser/posts/{post}/accept', [DashboardController::class, 'acceptPost'])->name('reviser.accept');
+    Route::post('/reviser/posts/{post}/reject', [DashboardController::class, 'rejectPost'])->name('reviser.reject');
 });
