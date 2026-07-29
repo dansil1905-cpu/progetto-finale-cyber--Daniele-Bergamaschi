1c70270ccd9d412b67b43050046492dbc3082dba
dansil1905-cpu <dan.sil1905@gmail.com>
2026-07-29 18:33:13 +0200

    creazione authcontroller

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AuthController;
 
-// Rotte Pubbliche
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 
-// Rotte per Utenti Autenticati (Scrittori)
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
 Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
     Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 });
 
-// Rotte protette per Revisori
 Route::middleware(['auth', 'revisore'])->group(function () {
     Route::get('/reviser/dashboard', [DashboardController::class, 'reviserDashboard'])->name('reviser.dashboard');
     Route::post('/reviser/posts/{post}/accept', [DashboardController::class, 'acceptPost'])->name('reviser.accept');
