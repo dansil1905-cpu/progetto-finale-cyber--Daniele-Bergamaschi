8fe49f9d929f681ef1c389e5f597ef4156ad7a1c
dansil1905-cpu <dan.sil1905@gmail.com>
2026-07-29 22:14:09 +0200

    aggiustato una rotta

--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,26 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
+Route::get("/", [PostController::class, "index"])->name("posts.index");
 
-Route::middleware(['guest'])->group(function () {
-    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
+Route::middleware(["guest"])->group(function () {
+    Route::get("/register", [AuthController::class, "showRegister"])->name("register");
+    Route::post("/register", [AuthController::class, "register"]);
+    Route::get("/login", [AuthController::class, "showLogin"])->name("login");
+    Route::post("/login", [AuthController::class, "login"]);
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware(["auth"])->group(function () {
+    Route::post("/logout", [AuthController::class, "logout"])->name("logout");
 
-    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get("/posts/create", [PostController::class, "create"])->name("posts.create");
+    Route::post("/posts", [PostController::class, "store"])->name("posts.store");
 });
 
-Route::middleware(['auth', 'revisore'])->group(function () {
-    Route::get('/reviser/dashboard', [DashboardController::class, 'reviserDashboard'])->name('reviser.dashboard');
-    Route::post('/reviser/posts/{post}/accept', [DashboardController::class, 'acceptPost'])->name('reviser.accept');
-    Route::post('/reviser/posts/{post}/reject', [DashboardController::class, 'rejectPost'])->name('reviser.reject');
+Route::get("/posts/{post:slug}", [PostController::class, "show"])->name("posts.show");
+
+Route::middleware(["auth", "revisore"])->group(function () {
+    Route::get("/reviser/dashboard", [DashboardController::class, "reviserDashboard"])->name("reviser.dashboard");
+    Route::post("/reviser/posts/{post}/accept", [DashboardController::class, "acceptPost"])->name("reviser.accept");
+    Route::post("/reviser/posts/{post}/reject", [DashboardController::class, "rejectPost"])->name("reviser.reject");
 });
